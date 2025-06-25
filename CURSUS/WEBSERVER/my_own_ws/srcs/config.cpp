df96afa2748203b64df9915e5b0110d646c28b4e
druiz-ca <danielruizcampos7@gmail.com>
2025-06-25 12:16:18 +0200

    actualizo ws

--- CURSUS/WEBSERVER/my_own_ws/srcs/config.cpp
+++ CURSUS/WEBSERVER/my_own_ws/srcs/config.cpp
@@ -3,7 +3,29 @@
 Config::Config(const string& filepath)
 {
     cerr << filepath << endl;
+    // tengo que llamar a parse desde aquí porque es un método privado
     parse(filepath);
+}
+
+/* Limpia:
+    - espacios y saltos de línea al cominezo y final
+    - puntos y coma al final si lo hay
+*/
+string Config::trim(const string& line)
+{
+    // Busca 1º char que no sea espacio, tab, o salto de l.
+        // si solo encuentra esapcio, tab, etc devuelve npos
+    size_t first = line.find_first_not_of(" \t\n\r");
+
+    // Si no encuentra ningún char útil (si llega hasta el final del string)
+    if (first == string::npos)
+        return ""; // devuelve cadena vacía
+
+    // busca ult. pos que no sea ....
+    size_t last = line.find_last_not_of(" \t\n\r");
+
+    // subst-> pos inicial, cantidad de chars
+    return line.substr(first, last - first + 1);
 }
 
 void Config::parse(const string& filepath)
@@ -15,4 +37,27 @@ void Config::parse(const string& filepath)
 
     if (!file.is_open())
         throw runtime_error("Could not open config file: " + filepath);
+    
+    string line;
+    while (getline(file, line))
+    {   // me limpia el string de espacios
+        line = trim(line);
+        if (line == "")
+            throw runtime_error("Error in filepath: not cointains a path");
+        if (line.empty() || line[0] == '#')
+            continue;
+        if (line.find("server") == 0 && line.find('{') != string::npos)
+        {
+            Server_config server;
+            parseServer(file, line, server);
+        }
+        else
+            throw runtime_error("Error: file not contains server bloque");
+    }
+    file.close();
+}
+
+void Config::parseServer(ifstream &file, string &line, Server_config& server)
+{
+
 }
