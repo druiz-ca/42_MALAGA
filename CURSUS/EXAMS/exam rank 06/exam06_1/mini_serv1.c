1abb864529c99dff370142f838c34c8ac77e5645
druiz-ca <danielruizcampos7@gmail.com>
2025-08-06 17:59:10 +0200

    subo practica del mw

--- CURSUS/EXAMS/exam rank 06/exam06_1/mini_serv1.c
+++ CURSUS/EXAMS/exam rank 06/exam06_1/mini_serv1.c
@@ -1,13 +1,31 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 
-#include <errno.h>
 #include <sys/socket.h>
-#include <sys/select.h>
+#include <errno.h>
 #include <netinet/in.h>
+#include <sys/select.h>
 
-#define MAX_CLIENTS 1024
-#define MAX_CLIENTS 1000000
+#define LONGITUD_MAX_MENSAJE 1000000
+#define CLIENTES_MAXIMOS 1024
+
+typedef struct s_client
+{
+    int id;
+	char msg[LONGITUD_MAX_MENSAJE];
+}t_client;
+
+t_client clientes[CLIENTES_MAXIMOS];
+
+fd_set write_set;
+fd_set read_set;
+fd_set fds_monitoreados;
+
+char send_buffer[LONGITUD_MAX_MENSAJE];
+char recv_buffer[LONGITUD_MAX_MENSAJE];
+
+int id_actual = 0;
+int max_num_fds = 0;
 
