0a68f460d06cf2241eb0d11699971e748cc4f4da
druiz-ca <danielruizcampos7@gmail.com>
2025-08-06 18:36:47 +0200

    amplio practica mw

--- CURSUS/EXAMS/exam rank 06/exam06_1/mini_serv1.c
+++ CURSUS/EXAMS/exam rank 06/exam06_1/mini_serv1.c
@@ -1,31 +1,53 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <errno.h>
 #include <netinet/in.h>
-#include <sys/select.h>
 
-#define LONGITUD_MAX_MENSAJE 1000000
-#define CLIENTES_MAXIMOS 1024
+#define CLIENTES_MAX 1024
+#define LONGITUD_MAX_MSG 1000000
 
 typedef struct s_client
 {
-    int id;
-	char msg[LONGITUD_MAX_MENSAJE];
+	int id;
+	char msg[LONGITUD_MAX_MSG];
 }t_client;
 
-t_client clientes[CLIENTES_MAXIMOS];
+t_client struct_clientes[CLIENTES_MAX];		
 
 fd_set write_set;
 fd_set read_set;
 fd_set fds_monitoreados;
 
-char send_buffer[LONGITUD_MAX_MENSAJE];
-char recv_buffer[LONGITUD_MAX_MENSAJE];
+char buffer_read[LONGITUD_MAX_MSG];
+char recv_buffer[LONGITUD_MAX_MSG];
 
-int id_actual = 0;
-int max_num_fds = 0;
+int fd_actual = 0;
+int max_fds = 0;
 
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		ft_err("Wrong number of arguments\n");
+	
+	int socket_fd = 0;
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_fd == -1)
+		ft_error (NULL);
+	FD_ZERO(&fds_monitoreados);
+	FD_SET(socket_fd, &fds_monitoreados);
+
+	struct sockaddr_in server_config;
+	bzero(&server_config, sizeof(server_config));
+	server_config.sin_addr.s_addr = AF_INET;
+	server_config.sin_family = inet_addr("127.0.0.1");
+	server_config.sin_port = htons(atoi(argv[1]));
+	
+	max_fds = socket_fd;
+
+	if ((bind(socket_fd, (const struct sockaddr*))))
+}
