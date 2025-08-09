855d3d76738f4116413653cc34b3079f39f6e2ea
druiz-ca <danielruizcampos7@gmail.com>
2025-08-09 15:51:34 +0200

    subo practica mw

--- CURSUS/EXAMS/exam rank 06/exam06_1/mini_serv1.c
+++ CURSUS/EXAMS/exam rank 06/exam06_1/mini_serv1.c
@@ -16,39 +16,38 @@ typedef struct s_client
 	int id;
 	char msg[MAX_SIZE_MSG];
 }t_client;
-t_client clients[MAX_CLIENTS];
+t_client clientes[MAX_CLIENTS];
 
-fd_set write_fd;
-fd_set read_fd;
+fd_set write_fds;
+fd_set read_fds;
 fd_set monitored_fds;
 
 char buffer_read[MAX_SIZE_MSG];
 char buffer_recv[MAX_SIZE_MSG];
 
-int max_fds = 0;
-int id_actual;
+int current_id = 0;
+int max_fd = 0;
 
 int main(int argc, char **argv)
 {
 	if (argc != 2)
-		ft_error("wrong");
-	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_fd == -1)
-		ft_error(NULL);
+		ft_error("Erorr");
+	int socket_fd = sock(AF_INET, SOCK_STREAM, 0);
 	FD_ZERO(&monitored_fds);
 	FD_SET(socket_fd, &monitored_fds);
-	
+
 	struct sockaddr_in server_config;
 	bzero(&server_config, sizeof(server_config));
-	server_config.sin_family = AF_INET;
-	server_config.sin_port = htons(atoi(argv[1]));
 	server_config.sin_addr.s_addr = inet_addr("127");
+	server_config.sin_port = htons(atoi(argv[1]));
+	server_config.sin_family = AF_INET;
 
-	max_fds = socket_fd;
+	max_fd = socket_fd;
 
-	if ((bind(&socket_fd, (const struct sockaddr*)&server_config, sizeof(server_config))) != 0)
+	if ((bind(socket_fd, (const struct sockaddr*)&server_config, sizeof(server_config))) != 0)
 		ft_error(NULL);
 	if (listen(socket_fd, 10) != 0)
 		ft_error(NULL);
-
+	
+	
 }
