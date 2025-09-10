79955c83516b50a02922bdad1452469d1a84f172
daniel <danielruizcampos7@gmail.com>
2025-09-10 20:07:28 +0200

    isntalo express

---
+++ CURSUS/TRASCENDENCE/backend/users/src/index.js
@@ -0,0 +1,6 @@
+// filepath: /home/druiz-ca/Documents/clone2/42_MALAGA/CURSUS/TRASCENDENCE/backend/users/src/index.ts
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.listen(3000, () => console.log('User microservice running on port 3000'));
+//# sourceMappingURL=index.js.map
