38df5ba17cb7a631d89ec8cf853b8f1c3d117f01
daniel <danielruizcampos7@gmail.com>
2025-09-10 19:26:57 +0200

    subo directorios trans

---
+++ CURSUS/TRASCENDENCE/backend/users/src/index.ts
@@ -0,0 +1,5 @@
+// filepath: /home/druiz-ca/Documents/clone2/42_MALAGA/CURSUS/TRASCENDENCE/backend/users/src/index.ts
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.listen(3000, () => console.log('User microservice running on port 3000'));
