6858dd0c29d58e8da9838c1d67082e017280bd88
daniel <danielruizcampos7@gmail.com>
2025-09-21 19:00:53 +0200

    subo el ejercicio 5:Usuarios concretos

---
+++ CURSUS/TRASCENDENCE/ejercicios/5.Usuarios_concretos/dist/index.js
@@ -0,0 +1,51 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+async function main() {
+    const fastify = Fastify();
+    await fastify.register(cors);
+    let arrayUsuarios = [];
+    fastify.post('/post', (solicitud, respuesta) => {
+        const { nombre, email } = solicitud.body;
+        arrayUsuarios.push({ nombre, email });
+        respuesta.send('Guardado');
+    });
+    fastify.get('/get', (solicitud, respuesta) => {
+        respuesta.send(arrayUsuarios);
+    });
+    fastify.get('/get/:nombre', (solicitud, respuesta) => {
+        const nombreBuscado = solicitud.params.nombre;
+        const usuario = arrayUsuarios.find(u => u.nombre === nombreBuscado);
+        if (usuario)
+            respuesta.send(usuario);
+        else
+            respuesta.status(404).send('No existe');
+    });
+    fastify.delete('/delete/:nombre', (solicitud, respuesta) => {
+        const nombreBuscado = solicitud.params.nombre;
+        const index = arrayUsuarios.findIndex(obj_user => obj_user.nombre === nombreBuscado);
+        if (index !== -1) {
+            arrayUsuarios.splice(index, 1);
+            respuesta.send('Usuario eliminado');
+        }
+        else
+            respuesta.status(404).send('No existe el nombre');
+    });
+    fastify.put('/put/:nombre', (solicitud, respuesta) => {
+        // Extraigo el nombre del parámetro de la solicitud (/put/:nombre)
+        const nombreBuscado = solicitud.params.nombre;
+        // Extraigo el campo email del body de la solicitud
+        const { email } = solicitud.body;
+        //Busco el usuario en el array
+        const usuario = arrayUsuarios.find(obj_user => obj_user.nombre === nombreBuscado);
+        if (!usuario)
+            return respuesta.status(404).send('No existe ese nombre');
+        //Actualizo el email
+        usuario.email = email;
+        respuesta.send('Email actualizado');
+    });
+    fastify.listen({ port: 3000 }, () => {
+        console.log('API escuchando');
+    });
+}
+main();
+//# sourceMappingURL=index.js.map
