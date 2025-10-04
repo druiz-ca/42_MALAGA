c3909446a2666842eae51104266394903e15ac53
daniel <danielruizcampos7@gmail.com>
2025-10-04 11:54:06 +0200

    temrino funcion buscar posts

--- CURSUS/TRASCENDENCE/ejercicios_avanzados/3.Relacion_entre_recursos/dist/index.js
+++ CURSUS/TRASCENDENCE/ejercicios_avanzados/3.Relacion_entre_recursos/dist/index.js
@@ -1,29 +1,19 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors';
-import {promises as fs} from 'fs';
-
-async function main(){
+import { promises as fs } from 'fs';
+async function main() {
     const fastify = Fastify();
     await fastify.register(cors, {
         methods: ['GET', 'POST', 'DELETE', 'PUT']
     });
-
-    interface interUsuarios{
-        nombre: string;
-        email: string;
-    }
-
-    async function leerUsuarios(){
+    async function leerUsuarios() {
         const datos = await fs.readFile('usuarios.json', 'utf-8');
         return JSON.parse(datos);
     }
-
-    let arrayUsuarios: interUsuarios[] = await leerUsuarios();
-
-    async function guardarUsuarios(arrayUsuarios: interUsuarios[]){
+    let arrayUsuarios = await leerUsuarios();
+    async function guardarUsuarios(arrayUsuarios) {
         await fs.writeFile('usuarios.json', JSON.stringify(arrayUsuarios, null, 2));
     }
-
     fastify.post('/post', async (solicitud, respuesta) => {
         // cuando extraes + de 1 parámetro necesitas {}
         // as inter... especifica q debe contener el objeto
@@ -32,33 +22,28 @@ async function main(){
         // lo desestructuras: {nombre, email}
         // útil para q compruebe q exactamente recibe esos parámtros
         // pq si es objeto = ... no controla lo que recibe!!
-        const objeto = solicitud.body as interUsuarios;
-            // para + seguridad, desestructurar -> {nombre, email}
-        arrayUsuarios.push(objeto); 
+        const objeto = solicitud.body;
+        // para + seguridad, desestructurar -> {nombre, email}
+        arrayUsuarios.push(objeto);
         await guardarUsuarios(arrayUsuarios);
         respuesta.send('Guardado');
-    })
-
+    });
     // Ft para botón CONSULTAR
-    fastify.get('/get', async(solicitud, respuesta) => {
+    fastify.get('/get', async (solicitud, respuesta) => {
         respuesta.send(arrayUsuarios);
-    })
-
-    fastify.listen({port:3000}, () => {
+    });
+    fastify.listen({ port: 3000 }, () => {
         console.log('API is listening');
-    })
-
-
+    });
     fastify.get('/posts/:nombre', async (solicitud, respuesta) => {
         // Solicitud.params equivale a :nombre + es de tipo string
         // Extrae el nombre (.nombre) del parámetro del objeto recibido 
         // en la solicitud
-        const nombreBuscado = (solicitud.params as {nombre:string}).nombre;
-
+        const nombreBuscado = solicitud.params.nombre;
         //Filtra los posts que pertenecen a este usuario
         const postsUsuario = arrayUsuarios.filter(post => post.nombre === nombreBuscado);
-        respuesta.send(postsUsuario);        
-    })
+        respuesta.send(postsUsuario);
+    });
 }
-
 main();
+//# sourceMappingURL=index.js.map
