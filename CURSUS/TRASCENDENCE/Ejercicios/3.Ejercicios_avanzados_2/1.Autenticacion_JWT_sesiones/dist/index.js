2b09833b20d633177ac95b84d73e1a4e4a58dc2e
daniel <danielruizcampos7@gmail.com>
2025-10-11 11:34:38 +0200

    temrino el punto B

--- CURSUS/TRASCENDENCE/Ejercicios/3.Ejercicios_avanzados_2/1.Autenticacion_JWT_sesiones/dist/index.js
+++ CURSUS/TRASCENDENCE/Ejercicios/3.Ejercicios_avanzados_2/1.Autenticacion_JWT_sesiones/dist/index.js
@@ -1,9 +1,56 @@
-import * as bcrypt from 'bcrypt';
-const password = 'miContraseñaSegura';
-const saltRounds = 10;
-// Hashear la contraseña
-bcrypt.hash(password, saltRounds, function (err, hash) {
-    // Guardar 'hash' en la base de datos
-    console.log(hash);
-});
+/* ----------------------------------------------------------------------
+                            IMPORTS Y CONFIGURACIÓN
+---------------------------------------------------------------------- */
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import bcrypt from 'bcrypt';
+import { promises as fs } from 'fs';
+import jwt from 'jsonwebtoken';
+async function main() {
+    const fastify = Fastify();
+    await fastify.register(cors, {
+        methods: ['GET', 'POST', 'DELETE', 'PUT']
+    });
+    async function leerUsuarios() {
+        const datos = await fs.readFile('usuarios.json', 'utf-8');
+        return JSON.parse(datos);
+    }
+    async function guardarUsuarios(arrayUsuarios) {
+        await fs.writeFile('usuarios.json', JSON.stringify(arrayUsuarios, null, 2));
+    }
+    let arrayUsuarios = await leerUsuarios();
+    fastify.post('/register', async (req, res) => {
+        const { nombre, email, password } = req.body;
+        // Validar duplicados...
+        const existe = arrayUsuarios.find(u => u.email === email || u.nombre === nombre);
+        if (existe)
+            return res.status(400).send('Ya existe');
+        // Hashear el password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // Guardar usuario con hashedPassword
+        arrayUsuarios.push({ nombre, email, password: hashedPassword });
+        await guardarUsuarios(arrayUsuarios);
+        //Envíamos mensaje de confirmación
+        res.send('Usuario registrado');
+    });
+    fastify.post('/login', async (req, res) => {
+        const { email, password } = req.body;
+        // Comprobar que el usuario (x mail) exista
+        const checkUser = arrayUsuarios.find(u => u.email === email);
+        if (!checkUser)
+            return res.status(400).send('No existe');
+        // Comparar el password recibido con el hasheado
+        const match = await bcrypt.compare(password, checkUser.password);
+        if (!match)
+            return res.status(401).send('Contraseña incorrecta');
+        // Generar un JWT con los datos del ususario (menos el pass)
+        const token = jwt.sign({ nombre: checkUser.nombre, email: checkUser.email }, 'clave_secreta', { expiresIn: '1h' });
+        // Enviar el token al Front en la respuesta
+        res.send({ token });
+    });
+    fastify.listen({ port: 3000 }, () => {
+        console.log('API is listening');
+    });
+}
+main();
 //# sourceMappingURL=index.js.map
--- CURSUS/TRASCENDENCE/Ejercicios/3.Ejercicios_avanzados_2/1.Autenticacion_JWT_sesiones/src/index.ts
+++ CURSUS/TRASCENDENCE/Ejercicios/3.Ejercicios_avanzados_2/1.Autenticacion_JWT_sesiones/src/index.ts
@@ -1,13 +1,81 @@
 /* ----------------------------------------------------------------------
                             IMPORTS Y CONFIGURACIÓN
 ---------------------------------------------------------------------- */
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
 import bcrypt from 'bcrypt';
+import {promises as fs} from 'fs';
+import jwt from 'jsonwebtoken';
 
-const saltRounds = 10;
-// Hasheado de contraseña
-const hashedPassword = await bcrypt.hash(password_noHash, saltRounds);
+async function main(){
+    const fastify = Fastify()
+    await fastify.register(cors, {
+        methods: ['GET', 'POST', 'DELETE', 'PUT']
+    });
 
-// LOGIN Compara la contraseña recibida con el hash guardado
-const match = await bcrypt.compare(password_noHash, usuario.password);
-if(!match) return respuesta.status(401).send('Credenciales incorrectas');
+    interface interUsuarios{
+        nombre: string,
+        email: string,
+        password: string
+    }
 
+    async function leerUsuarios(){
+        const datos = await fs.readFile('usuarios.json', 'utf-8');
+        return JSON.parse(datos);
+    }
+
+    async function guardarUsuarios(arrayUsuarios: interUsuarios[]){
+        await fs.writeFile('usuarios.json', JSON.stringify(arrayUsuarios, null, 2));
+    }
+    
+    let arrayUsuarios: interUsuarios[] = await leerUsuarios();
+
+    fastify.post('/register', async (req, res) => {
+        const { nombre, email, password } = req.body as { nombre: string, email: string, password: string };
+        
+        // Validar duplicados...
+        const existe = arrayUsuarios.find(u => u.email === email || u.nombre === nombre)
+        if (existe)
+            return res.status(400).send('Ya existe');
+
+        // Hashear el password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Guardar usuario con hashedPassword
+        arrayUsuarios.push({nombre, email, password: hashedPassword})
+        await guardarUsuarios(arrayUsuarios);
+
+        //Envíamos mensaje de confirmación
+        res.send('Usuario registrado');
+    });
+
+    fastify.post('/login', async (req, res) => {
+        const {email, password} = req.body as {email: string, password: string};
+
+        // Comprobar que el usuario (x mail) exista
+        const checkUser = arrayUsuarios.find(u => u.email === email);
+        if(!checkUser)
+            return res.status(400).send('No existe');
+
+        // Comparar el password recibido con el hasheado
+        const match = await bcrypt.compare(password, checkUser.password);
+        if (!match)
+            return res.status(401).send('Contraseña incorrecta');
+
+        // Generar un JWT con los datos del ususario (menos el pass)
+        const token = jwt.sign(
+            {nombre: checkUser.nombre, email: checkUser.email},
+            'clave_secreta', // clave con la que se firma el token
+            {expiresIn: '1h'} // el token caduca en 1 hora
+        );
+
+        // Enviar el token al Front en la respuesta
+        res.send({token});
+    });
+
+    fastify.listen({port:3000}, ()=> {
+        console.log('API is listening');
+    });
+}
+
+main();
